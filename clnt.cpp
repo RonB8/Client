9bd5ee5a6969325c6b6eb832c33199f5098cfa9b
RonB8 <ronbuaron8@gmail.com>
2024-09-09 13:50:29 +0300

    -

--- clnt.cpp
+++ clnt.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cerrno>
+#include <cstring>
 
 int main(){
 
-    std::cout << "GOL\n";
+    //client syntax
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    //address
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    //sending
+    const char* msg = "Hi dear client!";
+    send(clientSocket, msg, strlen(msg), 0)
+
+    //close socket
+     close(clientSocket);
+
+
     return 0;
 }
